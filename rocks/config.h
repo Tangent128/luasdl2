b6e58230b3904a937c79758dbfbb9947e2f579ab
Joseph Wallace <Tangent128@gmail.com>
2015-08-29 15:18:59 -0400

    Binding version 2.0.3.4-0 Merge some bugfixes by Christian Storm. First stab at removing CMake dependency when building with LuaRocks.

--- rocks/config.h
+++ rocks/config.h
@@ -1,7 +1,7 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
 
-#define	VERSION_BINDING	3
-#define	VERSION_BINDING_PATCH	1
+#define	VERSION_BINDING	4
+#define	VERSION_BINDING_PATCH	0
 
 #endif /* !_CONFIG_H_ */
